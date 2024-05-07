b2d76e44356707339c449ab21a425348064af9f2
Byron Salvador <byron.salvador@epn.edu.ec>
2024-05-06 22:39:52 -0500

    Fix: Footer.tsx problemas con el footer solucionados

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,14 +1,18 @@
-import { ScrollToSection } from "../../App";
+//import { ScrollToSection } from "../../App";
 import { useMenuNavbar } from "../../hooks/useMenuNavbar";
 import FooterStyles from "../Navbar/Navbar.module.css";
+//import FooterStyles from "./Footer.module.css";
+import { ScrollToSection } from "../../App";
 
 interface Pros {
     scrollToSection: ScrollToSection
 }
 
+
+
 export default function Footer({ scrollToSection }: Pros) {
     const currentDate = new Date().getFullYear();
-    const { items, changeSelectedItem } = useMenuNavbar()
+    //const { items, changeSelectedItem } = useMenuNavbar()
     const fullName = "Byron Salvador"
     return (
         <footer className="
@@ -16,22 +20,7 @@ export default function Footer({ scrollToSection }: Pros) {
         w-standard
         mx-auto
         ">
-            <nav className="flex justify-between p-5 border-t-2 mt-5 border-primary-900">
-                <button className="text-2xl">{fullName}</button>
-                <ul className="flex gap-10 text-base tracking-wide">
-                    {items.map((item) => {
-                        const { name, status } = item
-                        return (
-                            <li key={name} className={`h-20 ${FooterStyles.item_base} ${status ? FooterStyles.item_active : FooterStyles.item_inactive}`}>
-                                <button onClick={() => {
-                                    scrollToSection[name]()
-                                    changeSelectedItem(name)
-                                }}>{name}</button>
-                            </li>
-                        )
-                    })}
-                </ul>
-            </nav>
+            
             <ul className="flex justify-end gap-10 text-slate-100/40 font-semibold">
                 <li>&#169; {currentDate} {fullName}. All rights reserved</li>
                 <li>Terms of Use</li>
